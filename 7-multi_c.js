6c515871dfd8b8112baf13cc84e1f52baf908c8d
Amanuel <beginningoflife2324@gmail.com>
2025-07-10 05:09:23 -0700

    Multi printing with argument passing

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const num = process.argv[2];
+const x = Number(num);
+if (isNaN(x)){
+    console.log("Missing number of occurrences");
+
+}
+else{
+    for(i = 0; i <= x; i++){console.log("C is fun");}
+}
