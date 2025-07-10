4739bddd040c5f4b46bf02be0c779220047b4183
Amanuel <beginningoflife2324@gmail.com>
2025-07-10 04:46:43 -0700

    Passing and now the arguments if changable to number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,7 @@
-const arg = Process.argv[2];
-const number = number(arg);
-if (!isNaN(number)) {
-  console.log("My number:" + number);
-}
-else {
+const arg = process.argv[2];
+const num = parseInt(arg);
+if (!isNaN(num)) {
+  console.log("My number: " + num);
+} else {
   console.log("Not a number");
 }
