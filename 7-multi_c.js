e15c01f1957b70f489d60d053e56eb2676cc15f6
Amanuel <beginningoflife2324@gmail.com>
2025-07-10 05:38:54 -0700

    Printing with size of square

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,7 @@
 const num = process.argv[2];
-const x = Number(num);
-if (!isNaN(x)){
-    for(i = 0; i <= x; i++){console.log("C is fun");}
-
-}
-else{
-    
-    console.log("Missing number of occurrences");
+const x = parseInt(num);
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {n
+  for (let i = 0; i < x; i++) console.log("C is fun");
 }
